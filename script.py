e746cfb4a696e1def7bfafe1cf38459e40dc50ff
Math-ias <mathi4semail@gmail.com>
2017-02-20 21:22:09 -0500

    Remove geolocation from javascript and implement in python code.
    I removed geolocation fetching from the javascript code and instead
    chose to use a geolocation API, still from google, in python code.
    
    This is actually better because it cuts down the number of requests
    I need to make (only one set, instead of a big set for every client).
    
    What I was actually doing wrong for the geolocation library was that I
    wasn't requesting it with &libraries=geolocation or something when I was
    loading the library. Geolocation with javascript is a library for the google
    maps library so I needed to load it.
    
    https://developers.google.com/maps/documentation/javascript/tutorial

--- script.py
+++ script.py
@@ -1,16 +1,31 @@
 #/usr/bin/python3
 
 import requests
+import googlemaps
+import json
 
 JSON_OUTPUT = "teams.js"
 EVENTNAME = "2017marea"
+
+gmaps = googlemaps.Client(key='REDACTED')
 
 url = "https://www.thebluealliance.com/api/v2/event/{}/teams".format(EVENTNAME)
 headers = {'X-TBA-App-Id':'4761:2017mareaMap:1'}
 
 r = requests.get(url, headers=headers)
 
+myjson = r.json()
+
+event = gmaps.geocode('62 Oakland Rd, Reading, MA 01867')[0]["geometry"]["location"]
+
+for team in myjson:
+	geocode_result = gmaps.geocode(team["location"])[0]
+	team["position"] = geocode_result["geometry"]["location"]
+
 with open(JSON_OUTPUT, 'w') as myfile:
 	myfile.write("teams = ")
-	myfile.write(r.text) # Save the results.
+	json.dump(myjson, myfile) # Save the results.
+	myfile.write(";")
+	myfile.write("event = ")
+	json.dump(event, myfile)
 	myfile.write(";")
