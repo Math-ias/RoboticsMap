1d710d0cf28d09ce43a294a34d43bcb51affae85
Math-ias <mathi4semail@gmail.com>
2017-02-22 17:54:19 -0500

    Add new bounds for the map, make map full width, and run script.py..

--- map.js
+++ map.js
@@ -1,18 +1,64 @@
 function initMap() {
-	// Initialize services.
+	// Initialize symbols.
+	var goldStar = { 
+		path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
+		fillColor: 'red',
+		fillOpacity: 1,
+		scale: 0.095,
+		strokeColor: 'black',
+		strokeWeight: 1,
+};
+
+	var hat_image = 'hat.png';
+
+	/*
+	var hatIcon = { 
+		url: hat_image,
+		size: new google.maps.Size(30,24),
+		origin: new google.maps.Point(0, 0), 
+		anchor: new google.maps.Point(10, 9), 
+	};*/
+
+	/*
+	var hat_shape = {
+		coords: [1, 1, 1, 18, 20, 18, 20, 1],
+		type: 'poly',
+	};*/
 
 	// Create map and place markers.
 	var map = new google.maps.Map(document.getElementById('map'), {
-        	zoom: 6,
-        	center: event,
-        });
+        // My controls.
+		zoom: 10,
+		mapTypeControl: false,
+		streetViewControl: true,
+		fullscreenControl: true,
+		// Automatic.
+        center: event,
+    });
 
+	map.fitBounds(new google.maps.LatLngBounds(boundsmin,boundsmax));
+
+	// Event marker.	
+
+	var event_marker = new google.maps.Marker({
+		position: event,
+		icon: goldStar,
+		map: map,
+		label: "R",
+	});
+
+	// Team markers.
 	for (var i=0; i<teams.length; i=i+1) {
 		team = teams[i];
 		if (team["position"] != null) {
 			var marker = new google.maps.Marker({
-        			position: team["position"],
+					position: team["position"],
         			map: map,
+					label: team['nickname'][0],
+					/*icon: hatIcon,
+					shape: hat_shape,
+					title: team['nickname'],
+					zIndex: 1,*/
 			});
 		}
 		
--- script.py
+++ script.py
@@ -18,9 +18,30 @@ myjson = r.json()
 
 event = gmaps.geocode('62 Oakland Rd, Reading, MA 01867')[0]["geometry"]["location"]
 
+# Gather geocoding results for each team address. And determine bounds of all teams.
+
+minlat = event["lat"]
+minlng = event["lng"]
+maxlat = minlat
+maxlng = minlng
+
 for team in myjson:
 	geocode_result = gmaps.geocode(team["location"])[0]
 	team["position"] = geocode_result["geometry"]["location"]
+	lat = geocode_result["geometry"]["location"]["lat"]
+	lng = geocode_result["geometry"]["location"]["lng"]
+	if lat > maxlat:
+		maxlat = lat
+	elif lat < minlat:
+		minlat = lat
+
+	if lng > maxlng:
+		maxlng = lng
+	elif lat < minlng:
+		minglng = lng
+
+boundsmin = {"lat": minlat, "lng": minlng}
+boundsmax = {"lat": maxlat, "lng": maxlng}
 
 with open(JSON_OUTPUT, 'w') as myfile:
 	myfile.write("teams = ")
@@ -28,4 +49,10 @@ with open(JSON_OUTPUT, 'w') as myfile:
 	myfile.write(";")
 	myfile.write("event = ")
 	json.dump(event, myfile)
+	myfile.write(";")
+	myfile.write("boundsmin = ")
+	json.dump(boundsmin, myfile)
+	myfile.write(";")
+	myfile.write("boundsmax = ")
+	json.dump(boundsmax, myfile)
 	myfile.write(";")
