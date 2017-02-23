9ebddf2b6f970c8f20d5bbd2d8fd33d17bbbc403
Math-ias <mathi4semail@gmail.com>
2017-02-22 19:04:49 -0500

    Use one infowindow. Create a listener differently.
    I use just one InfoWindow and close it everytime I listen to a click. This
    ensures I only have one window open at once. Prevent clutter.

--- map.js
+++ map.js
@@ -1,4 +1,16 @@
 function initMap() {
+	// Functions.
+
+	var infoWindow = new google.maps.InfoWindow();
+
+	function info_window(marker,mymap,content){
+		google.maps.event.addListener(marker, 'click', function() {
+			infoWindow.setContent(content);
+			infoWindow.close();
+			infoWindow.open(mymap, marker);
+		});
+	}
+
 	// Initialize symbols.
 	var goldStar = { 
 		path: 'M 125,5 155,90 245,90 175,145 200,230 125,180 50,230 75,145 5,90 95,90 z',
@@ -7,7 +19,7 @@ function initMap() {
 		scale: 0.095,
 		strokeColor: 'black',
 		strokeWeight: 1,
-};
+	};
 
 	var hat_image = 'hat.png';
 
@@ -47,21 +59,33 @@ function initMap() {
 		label: "R",
 	});
 
-	// Team markers.
+	// Team markers and info windows.
 	for (var i=0; i<teams.length; i=i+1) {
 		team = teams[i];
 		if (team["position"] != null) {
 			var marker = new google.maps.Marker({
-					position: team["position"],
-        			map: map,
-					label: team['nickname'][0],
-					/*icon: hatIcon,
-					shape: hat_shape,
-					title: team['nickname'],
-					zIndex: 1,*/
+				position: team["position"],
+        		map: map,
+				label: team['nickname'][0],
+				/*icon: hatIcon,
+				shape: hat_shape,
+				title: team['nickname'],
+				zIndex: 1,*/
 			});
-		}
-		
+
+			if (team["motto"] == null) { team["motto"]=""; }
+
+			var content_string = '<div id="content">'+
+				'<div id="siteNotice"></div>"'+
+				'<h1> Team '+team['team_number']+' '+team['nickname']+'</h1>'+
+				'<h3>'+team['name']+'</h3>'+
+				'<p>('+team['rookie_year']+'-present) '+team['motto']+
+				'</p>'+
+				'<p><a href="'+team['website']+'">Team website</a></p>'+
+				'</div>';
+
+			info_window(marker, map, content_string);
+		}	
 	}
 }
 
